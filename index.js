f85808f69be199c7f7270e4a6c5832a499ff2e58
amllan123 <99805797+amllan123@users.noreply.github.com>
2023-11-10 17:47:37 +0530

    "Lock logic implementation"

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-let isLightOn = false;
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
-    res.send("Heloo ji")
+let isLocked = true; // Initial state is locked
+const correctPassword = '123'; // Replace with your actual password
+app.get('/', (req, res) =>{
+    res.send("Hello")
 })
-app.get('/light/on', (req, res) => {
-  isLightOn = true;
-  res.send('Light is now on');
+app.get('/lock/state', (req, res) => {
+  res.json({ isLocked });
 });
 
-app.get('/light/off', (req, res) => {
-  isLightOn = false;
-  res.send('Light is now off');
+app.post('/lock/unlock', (req, res) => {
+  const { password } = req.body;
+
+  if (password === correctPassword) {
+    isLocked = false;
+    res.json({ success: true, isLocked });
+  } else {
+    res.status(401).json({ success: false, message: 'Incorrect password' });
+  }
 });
 
-app.get('/status', (req, res) => {
-  res.send({ isLightOn });
+app.post('/lock/lock', (req, res) => {
+  isLocked = true;
+  res.json({ success: true, isLocked });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
